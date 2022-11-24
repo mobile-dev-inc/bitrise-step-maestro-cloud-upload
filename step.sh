2b18eecdee9a2a2a5242257c9e23ea9619044fb8
Arthur Saveliev <arthur.saveliev@gmail.com>
2022-11-24 12:22:16 +0100

    first commit

---
+++ step.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Parse env variables
+envs=$(echo $env | tr "\n" "\n")
+env_list=""
+for e in $envs
+do
+    env_list+="-e $e "
+done
+
+
+set -ex
+
+# Async
+[ "$async" == "true" ] && is_async="true"
+
+cd $BITRISE_SOURCE_DIR
+
+# Install maestro CLI
+curl -Ls "https://get.maestro.mobile.dev" | bash
+export PATH="$PATH":"$HOME/.maestro/bin"
+
+maestro cloud \
+--apiKey $api_key \
+${branch:+--branch "$branch"} \
+${repo_name:+--repoName "$repo_name"} \
+${repo_owner:+--repoOwner "$repo_owner"} \
+${mapping_file:+--mapping "$mapping_file"} \
+${upload_name:+--name "$upload_name"} \
+${is_async:+--async} \
+${pull_request_id:+--pullRequestId "$pull_request_id"} \
+${env_list:+ $env_list} \
+$app_file $workspace
