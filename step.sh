e125d2721393c80f1efa44d8bd857df28126c138
Arthur Saveliev <arthur.saveliev@gmail.com>
2022-11-24 11:07:14 +0100

    updates step

--- step.sh
+++ step.sh
@@ -22,12 +22,12 @@ export PATH="$PATH":"$HOME/.maestro/bin"
 
 maestro cloud \
 --apiKey $api_key \
-${BITRISE_GIT_BRANCH:+--branch "$BITRISE_GIT_BRANCH"} \
-${BITRISEIO_GIT_REPOSITORY_SLUG:+--repoName "$BITRISEIO_GIT_REPOSITORY_SLUG"} \
-${BITRISEIO_GIT_REPOSITORY_OWNER:+--repoOwner "$BITRISEIO_GIT_REPOSITORY_OWNER"} \
+${branch:+--branch "$branch"} \
+${repoName:+--repoName "$repoName"} \
+${repoOwner:+--repoOwner "$repoOwner"} \
 ${mapping_file:+--mapping "$mapping_file"} \
 ${upload_name:+--name "$upload_name"} \
 ${is_async:+--async} \
-${pullRequestId:+--pullRequestId "$BITRISE_PULL_REQUEST"} \
+${pullRequestId:+--pullRequestId "$pullRequestId"} \
 ${env_list:+ $env_list} \
 $app_file $workspace
